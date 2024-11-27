d03404d4e66cca095976d35547398b860747c1af
Александр Задорожный <zadorozhniy76@gmail.com>
2024-11-27 14:38:28 +0300

    fake_true

---
+++ module_4_2/test.py
@@ -0,0 +1,12 @@
+
+def test_function():
+    def inner_function ():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+
+# Ошибка, т.к. inner_function является локальной  и действует исключительно в пределах
+#функции test_function()
+#inner_function()
+
+
