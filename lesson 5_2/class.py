a4ec6fc43786c775b9bfc54d6bc0ee4e215449e6
Александр Задорожный <zadorozhniy76@gmail.com>
2024-12-09 14:12:19 +0300

    house

--- lesson 5_2/class.py
+++ lesson 5_2/class.py
@@ -1,15 +1,19 @@
 class House:
-    def __init__(self, self_name,number_of_floor):
-        self.name = self_name
+    def __init__(self, name,number_of_floor):
+        self.name = name
         self.number_of_floor = number_of_floor
 
-    def go_to (self,new_floor):
-        if new_floor > self.number_of_floor  or new_floor < 1:
+    def go_to(self,new_floor):
+        if new_floor > self.number_of_floor or new_floor < 1:
             print('Такого этажа не существует')
         else:
             for i in range(0,new_floor):
                 print(i+1)
+    def __len__(self):
+        return self.number_of_floor
 
+    def __str__(self):
+        return f'Название : {self.name}, количество этажей:{self.number_of_floor}'
 
 h1 = House('ЖК Горский', 18)
 
@@ -18,4 +22,16 @@ h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 
 h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+
+h2 = House('ЖК Акация', 20)
+
+
+print(h1)
+
+print(h2)
+
+print(len(h1))
+
+print(len(h2))
 
